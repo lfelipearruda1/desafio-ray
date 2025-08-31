742c1c6c22dcbd683aa045c440533889143219ae
lfelipearruda1 <lfelipearrudacc@gmail.com>
2025-08-31 11:51:31 -0300

    correção de armazenamento otimizado

--- sql/armazenamento_otimizado.sql
+++ sql/armazenamento_otimizado.sql
@@ -4,6 +4,7 @@ USE SCHEMA default;
 DROP TABLE IF EXISTS dados_consolidados_delta;
 CREATE TABLE dados_consolidados_delta
 USING DELTA
+PARTITIONED BY (origem)
 AS
 SELECT origem, id, data, cidade_std, tipo_maquina, revendedor_id, valor_total, needs_review
 FROM dados_consolidados;
@@ -11,12 +12,17 @@ FROM dados_consolidados;
 DROP TABLE IF EXISTS metricas_locais_delta;
 CREATE TABLE metricas_locais_delta
 USING DELTA
+PARTITIONED BY (cidade_padronizada)
 AS
-SELECT * FROM metricas_locais;
+SELECT *
+FROM metricas_locais;
 
 OPTIMIZE dados_consolidados_delta ZORDER BY (data, cidade_std);
-OPTIMIZE metricas_locais_delta    ZORDER BY (cidade_padronizada);
+OPTIMIZE metricas_locais_delta    ZORDER BY (qtd_vendas, total_vendas);
 
-SELECT 'dados_consolidados_delta' AS tabela, COUNT(*) AS linhas FROM dados_consolidados_delta
+SELECT 'delta_consolidados' AS tabela, COUNT(*) AS linhas FROM dados_consolidados_delta
 UNION ALL
-SELECT 'metricas_locais_delta'    AS tabela, COUNT(*) AS linhas FROM metricas_locais_delta;
+SELECT 'delta_metricas'     AS tabela, COUNT(*) FROM metricas_locais_delta;
+
+DESCRIBE DETAIL dados_consolidados_delta;
+DESCRIBE DETAIL metricas_locais_delta;
